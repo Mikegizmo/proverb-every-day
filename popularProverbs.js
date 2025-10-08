d28680a813e693534fee953658e5ea3887f1b009
Michael Panter <makinglife1231@gmail.com>
2025-10-08 12:33:16 -0400

    added popularProverbs.js file and impprting into script.js

---
+++ popularProverbs.js
@@ -0,0 +1,12 @@
+export const popularProverbs = [
+  {
+    "verse": "Proverbs 4:23",
+    "text": "Guard your heart with all diligence,\nfor from it flows springs of life."
+  },
+  {
+    "verse": "Proverbs 1:7",
+    "text": "The fear of the LORD is the beginning of knowledge,\nbut fools despise wisdom and discipline."
+  }
+]
+
+
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { popularProverbs } from "./popularProverbs.js";
+
 const clockwiseOrder = [
   "1","2","3","4","5","6","7",     // top
   "8","9","9","10","11","12","13","14","15", // right
@@ -136,3 +138,4 @@ proverbDay.addEventListener("click", async () => {
   }
 });
 
+console.log(popularProverbs);
